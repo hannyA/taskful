b9960056c8f3d09336b8352018bb944af75fe506
hannya <hannyaly2@gmail.com>
2023-01-05 20:33:35 -0500

    refactor: Moved home file to views

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("home");
 });
 
 app.get("/test", function (req, res) {
