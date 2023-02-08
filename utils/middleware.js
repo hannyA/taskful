25c9d25fe0a66c81901e2c7fe1c7783e44fc6478
hannya <hannyaly2@gmail.com>
2023-02-08 05:34:36 -0500

    feat: UI show login/logout buttons if user is signed in

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+  console.log("Islogged in currrent user:", req.user);
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be signed in to view this page!");
     return res.redirect("/auth/login");
