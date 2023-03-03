2a3e81dfeb6da88ab8fcfdf8a243bcf0499201cb
hannya <hannyaly2@gmail.com>
2023-03-02 22:40:01 -0500

    feat: Handle server shutdown

--- server.js
+++ server.js
@@ -26,7 +26,22 @@ async function main() {
 // });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function (req, res) {
+const server = app.listen(port, function (req, res) {
   console.log(`App running on port: ${port}...`);
   console.log("Server started up");
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!!! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECEIVED!, Shutting down gracefully!");
+  server.close(() => {
+    console.log("Process terminated!");
+  });
 });
