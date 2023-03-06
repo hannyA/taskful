18ce8c61f7eaab28d7bda66a9055fdca04a1f486
hannya <hannyaly2@gmail.com>
2023-03-05 23:31:00 -0500

    fix: Fix minutes in lastupdatetime to show double digits

--- models/issue.js
+++ models/issue.js
@@ -44,6 +44,7 @@ const formatTime = (date) => {
   let h = date.getHours() % 12;
   let end = date.getHours() >= 12 ? "pm" : "am";
   let min = date.getMinutes();
+  min = min < 10 ? `0${min}` : min;
   return `${h}:${min}${end}`;
 };
 
