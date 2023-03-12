4e6a2a1d88736c42d5cfeeeeb5874f9c884fae16
hannya <hannyaly2@gmail.com>
2023-03-12 15:02:36 -0400

    fix: 12pm shows correctly

--- utils/modelUtils.js
+++ utils/modelUtils.js
@@ -5,9 +5,12 @@ module.exports.formatDate = (date) => {
   return `${m}/${d}/${y}`;
 };
 module.exports.formatTime = (date) => {
-  let h = date.getHours() % 12;
-  let end = date.getHours() >= 12 ? "pm" : "am";
-  let min = date.getMinutes();
-  min = min < 10 ? `0${min}` : min;
-  return `${h}:${min}${end}`;
+  let hours = date.getHours();
+  let end = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours > 0 ? hours : 12;
+
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${hours}:${minutes}${end}`;
 };
