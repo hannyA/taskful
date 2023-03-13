039c77575e750a1aa3a9574b0c54491ab0bf1532
hannya <hannyaly2@gmail.com>
2023-03-13 13:43:50 -0400

    feat: Don't worry about it

--- seeds/delete.js
+++ seeds/delete.js
@@ -1,8 +1,28 @@
 const mongoose = require("mongoose");
+const path = require("path");
+
 const { deleteDB } = require("./app");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname, "../config.env") });
+
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/issue-tracker", {});
+
+// Get dev/prod url
+let localDB = process.env.DATABASE_LOCAL;
+console.log("localDB: ", localDB);
+
+let remoteDB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+).replace("<USERNAME>", process.env.DATABASE_USERNAME);
+console.log("remoteDB: ", remoteDB);
+
+let mongoDB = process.env.NODE_ENV === "development" ? localDB : remoteDB;
+
+console.log("mongoDB: ", mongoDB);
+
+mongoose.connect(mongoDB, {});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
